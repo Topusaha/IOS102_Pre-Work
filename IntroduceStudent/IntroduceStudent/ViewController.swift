b849f4e6c5f3f960733d2189688bc6b82bbe727e
Topu Saha <topusaha@Topus-MacBook-Air.local>
2023-12-26 16:18:28 -0500

    Layout Completed

--- IntroduceStudent/IntroduceStudent/ViewController.swift
+++ IntroduceStudent/IntroduceStudent/ViewController.swift
@@ -8,9 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var universityPhoto: UIImageView!
+    
+    // Text Fields
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolTextField: UITextField!
+    
+    
+    @IBOutlet weak var gradeLevelSlider: UISegmentedControl!
+    @IBOutlet weak var numberOfPetsTextField: UITextField!
+    @IBOutlet weak var numberOfPetsController: UIStepper!
+    @IBOutlet weak var morePetsController: UISwitch!
+    @IBOutlet weak var IntroduceButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         
     }
 
