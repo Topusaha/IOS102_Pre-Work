cc29e4b0f4e6515718b7ed142f5e281532aa9c75
Topu Saha <topusaha@Topus-MacBook-Air.local>
2023-12-26 16:43:40 -0500

    Pre-Work Completed

--- IntroduceStudent/IntroduceStudent/ViewController.swift
+++ IntroduceStudent/IntroduceStudent/ViewController.swift
@@ -29,10 +29,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
     }
+    
+    @IBAction func IntroduceSelf(_ sender: UIButton) {
+        
+        let year = gradeLevelSlider.titleForSegment(at: gradeLevelSlider.selectedSegmentIndex)
+        var introduction = "Hi, my name is \(firstNameTextField.text!) \(lastNameTextField.text!). I go to \(schoolTextField.text!) and I am a \(year!) year student. "
+        
+        if (Int(numberOfPetsTextField.text!)) != 0 {
+            introduction += "I have \(numberOfPetsTextField.text!) pets. "
+        }
+        
+        else {
+            introduction += "I currently have no pets. "
+        }
+        
+        if morePetsController.isOn {
+           introduction += "I would like to own more pets. "
+        }
+        
+        else {
+            introduction += "I would not like to own more pets. "
+        }
+        
+        let alertMessage = UIAlertController(title: "Introducting \(firstNameTextField.text!) \(lastNameTextField.text!)", message: introduction, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+        
+        alertMessage.addAction(action)
+        present(alertMessage, animated: true, completion: nil)
 
+    }
+    
+    @IBAction func didStepperChange(_ sender: UIStepper) {
+        numberOfPetsTextField.text = "\(Int(sender.value))"
+    }
+    
 
 }
 
